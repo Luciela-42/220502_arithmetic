af8f97e595c444d3f22fed01387b2f49842e54be
Hvvi_LCLA <inhui99@gmail.com>
2022-05-02 09:48:27 +0900

    20220502 four arithmetic f

--- main.cpp
+++ main.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 int main()
 {
-	int a = 3, b = 2;
+	int apple;
+	int ball;
+	apple = 4;
+	ball = 7;
 
-	float c = 7.9f, d = 4.2f;
+	float cash;
+	float devcat;
+	cash = 7.9f;
+	devcat  = 4.2f;
 
 	cout << "정수 사칙연산" << endl;
-	cout << a + b << endl;
-	cout << a - b << endl;
-	cout << a * b << endl;
-	cout << a / b << endl;
-	cout << a % b << endl;
+	cout << apple + ball << endl;
+	cout << apple - ball << endl;
+	cout << apple * ball << endl;
+	cout << apple / ball << endl;
+	cout << apple % ball << endl;
 
 	cout << "실수 사칙연산" << endl;
-	cout << c + d << endl;
-	cout << c - d << endl;
-	cout << c * d << endl;
-	cout << c / d << endl;
-	//cout << c % d << endl; %연산자는 정수형에서 사용
+	cout << cash + devcat << endl;
+	cout << cash - devcat << endl;
+	cout << cash * devcat << endl;
+	cout << cash / devcat << endl;
+	//cout << cash % devcat << endl; %연산자는 정수형에서 사용
 
 	return 0;
 }
